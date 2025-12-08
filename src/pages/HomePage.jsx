35203bc55c1ca41ab6bff1aadae050cc22590d05
leemageorge555 <leemageorge555@gmail.com>
2025-12-08 08:07:00 +0530

    updated height

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
 
   return (
     <div className="min-h-screen flex justify-center bg-gray-200 overflow-y-hidden">
-      <div className="max-w-[430px] w-full h-screen bg-gray-100 shadow-[0_0_20px_rgba(0,0,0,0.2)] overflow-y-scroll relative">
+      <div className="max-w-[430px] w-full h-full bg-gray-100 shadow-[0_0_20px_rgba(0,0,0,0.2)] overflow-y-scroll relative">
         <Header openPopup={handleOpenPopup} />
         {/* <Content /> */}
        {/* <UserPersonal />
